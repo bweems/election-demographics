caf8f843fa05810db86fb703cb6ebb1b99faa70d
joeligy <popps1992>
2014-11-08 14:56:16 -0800

    Can now add features to design matrix easily.

--- demographic_data_extraction.py
+++ demographic_data_extraction.py
@@ -4,8 +4,8 @@ import numpy as np
 
 column_names = []
 
-with open('demographic_csvs/ACS_07_1YR_DP2_with_ann.csv', 'rb') as csvfile:
-	spamreader = csv.reader(csvfile)
+
+def construct_design_matrix(spamreader):
 
 	designArray = []
 
@@ -23,8 +23,18 @@ with open('demographic_csvs/ACS_07_1YR_DP2_with_ann.csv', 'rb') as csvfile:
 			# Add percentage of households with children under 18 years
 			nextFeatureVector.append(row[13])
 
+			# Add percentage of households with a person 65 years or older
+			nextFeatureVector.append(row[57])
+
 			designArray.append(nextFeatureVector)
 
-	designMatrix = np.matrix(designArray)
+	return np.matrix(designArray)
+
+
+with open('demographic_csvs/ACS_07_1YR_DP2_with_ann.csv', 'rb') as csvfile:
+	spamreader = csv.reader(csvfile)
+
+	designMatrix = construct_design_matrix(spamreader)
+
 	print designMatrix
 
