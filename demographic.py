01c8553c91b2cb8f24a7adead85214c55fa35b90
joeligy <popps1992>
2014-12-02 17:10:56 -0600

    Fixed data issue.

--- demographic.py
+++ demographic.py
@@ -2,7 +2,29 @@ import csv
 import numpy as np
 import voting
 
+
+def modify_demographics(demographics, year):
+    new_dem_array = []
+    if year >= 2012:
+        for dem in demographics:
+            if dem.startswith('HC01_EST'):
+                new_dem_array.append(dem.replace('HC01_EST', 'HC01'))
+            elif dem.startswith('HC01_MOE'):
+                new_dem_array.append(dem.replace('HC01_MOE', 'HC02'))
+            elif dem.startswith('HC02_EST'):
+                new_dem_array.append(dem.replace('HC02_EST', 'HC03'))
+            elif dem.startswith('HC02_MOE'):
+                new_dem_array.append(dem.replace('HC02_MOE', 'HC04'))
+            else:
+                new_dem_array.append(dem)
+    else:
+        new_dem_array = demographics
+    return new_dem_array
+
 def construct_submatrix(year, demographics, county, prop, type, polarity):
+
+    demographics = modify_demographics(demographics, year)
+
     voting_data = voting.get_voting_data()
     targetArray = []
     vote = voting_data[str(year)][county + "_" + type][str(year) + "_" + prop + "_" + polarity]
